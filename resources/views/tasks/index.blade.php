7518783615988312bc4008aa16eb01b11a974612
Stefan Schneiders <PvT_Shifty@hotmail.com>
2019-04-12 14:10:43 +0200

    first commit

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,24 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.1.0/fullcalendar.min.css">
+
+<h3>Calendar</h3>
+
+<div id="calendar"></div>
+
+<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.17.1/moment.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.1.0/fullcalendar.min.js'></script>
+<script>
+    $(document).ready(function() {
+        $('#calendar').fullCalendar({
+            events : [
+                @foreach($tasks as $task)
+                {
+                    title : '{{ $task->name }}',
+                    start : '{{ $task->task_date }}',
+                    url : '{{ route('tasks.edit', $task->id) }}'
+                },
+                @endforeach
+            ]
+        })
+    });
+</script>
